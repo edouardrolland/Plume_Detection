95de777093b7817638183ef2770b271839958a99
Edouard Rolland <65714311+edouardrolland@users.noreply.github.com>
2024-11-26 07:58:11 +0100

    Remove unused files and add new inference script for YOLO model

---
+++ yolo_training.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+import json
+from pathlib import Path
+import torch
+from torch.utils.data import DataLoader
+from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
+import numpy as np
+from ultralytics import YOLO
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+# Load the YOLO model
+yolo_model = YOLO("/home/edouard/workspace/Plume_Detection/runs/detect/train/weights/best.pt")
+
+
+"""
+# Train the YOLO model
+yolo_model.train(
+    data=f"/home/edouard/workspace/Plume_Detection/notebooks/data.yaml",
+    epochs=300,
+    imgsz=640,
+    plots=True,
+    device="0",
+)
+"""
+
+# Validate the YOLO model
+yolo_model.val(
+    data=f"/home/edouard/workspace/Plume_Detection/notebooks/data.yaml",
+)
+
