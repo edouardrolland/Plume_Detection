d3b663542b3e7a71407168ae4245c50907b9850e
Edouard Rolland <65714311+edouardrolland@users.noreply.github.com>
2024-11-26 13:01:48 +0100

    Update model paths in model compression script for YOLO

--- model_compression.py
+++ model_compression.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a YOLOv8n PyTorch model
-model = YOLO("/home/edr/workspace/Plume_Detection/YOLO_Models/plume.pt")
+#model = YOLO("/home/edr/workspace/Plume_Detection/YOLO_Models/plume.pt")
 
 # Export the model
-model.export(format="imx", data = "/home/edr/workspace/Plume_Detection/data.yaml")  # exports with PTQ quantization by default
+#model.export(format="imx", data = "/home/edr/workspace/Plume_Detection/data.yaml")  # exports with PTQ quantization by default
 
 # Load the exported model
-imx_model = YOLO("yolov8n_imx_model")
+imx_model = YOLO("/home/edr/workspace/Plume_Detection/YOLO_Models/plume_imx_model")
 
